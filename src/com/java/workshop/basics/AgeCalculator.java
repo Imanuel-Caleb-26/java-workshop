4d1918a8bfd7d422f0fdea70677625e3302efc8a
Imanuel-Caleb-26 <imanuelcaleb3@gmail.com>
2025-07-08 10:53:54 +0530

    time

--- src/com/java/workshop/basics/AgeCalculator.java
+++ src/com/java/workshop/basics/AgeCalculator.java
@@ -6,13 +6,17 @@ public class AgeCalculator {
 
     public static void main(String[] args){
          Scanner scan = new Scanner(System.in);
-         System.out.println("Enter your of birth year: ");
-         int dob = scan.nextInt();
-         int currentYear = LocalDate.now().getYear();
-         int age = currentYear - dob;
-         System.out.println("The age is: " + age );
 
-        
 
+           System.out.println("Enter your of birth year: ");
+           String bday = scan.nextLine();
+           LocalDate currentYear1 = LocalDate.parse(bday);
+           int currentYear2 = currentYear1.getYear();
+           int currentYear = LocalDate.now().getYear();
+           int age1 = currentYear - currentYear2;
+           System.out.println("The age is: " + age1 );
+              
+
+  
     }
 }
