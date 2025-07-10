15b65b20c1605b067ed174a9a402d56595c04e49
Imanuel-Caleb-26 <imanuelcaleb3@gmail.com>
2025-07-10 15:55:47 +0530

    collection

---
+++ src/main/java/com/java/workshop/collection/StudentMap.java
@@ -0,0 +1,19 @@
+package com.java.workshop.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentMap {
+    public static void main(String[] args) {
+        Map<Integer, String> students = new HashMap<>();
+        students.put(1001, "Imanuel");
+        students.put(1002, "Shahul");
+        students.put(1003, "Zammer");
+        students.put(1004, "Santhosh");
+        
+        System.out.println(students);
+
+        System.out.println(students.keySet());
+        System.out.println(students.values());
+    }
+}
