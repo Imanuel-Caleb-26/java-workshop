2eea80c88f0ee1ba0e7eaccfedade49d63771173
Imanuel-Caleb-26 <imanuelcaleb3@gmail.com>
2025-07-14 10:37:58 +0530

    orgranisation

---
+++ src/main/java/com/java/workshop/temp/bin/Orgranaization.java
@@ -0,0 +1,11 @@
+package com.java.workshop.temp.bin;
+
+import java.time.LocalDate;
+
+public record Orgranaization( String name, String address, int contactNumber, 
+                             String email, String website,int registrationNumber,
+                              LocalDate registrationDate) {
+
+   
+
+}
--- src/main/java/com/java/workshop/temp/bin/TempdataApplication.java
+++ src/main/java/com/java/workshop/temp/bin/TempdataApplication.java
@@ -1,12 +1,24 @@
 package com.java.workshop.temp.bin;
+import com.java.workshop.temp.bin.Orgranaization;
+import java.time.LocalDate;
 
-import com.java.workshop.temp.bin.Location;
-import com.java.workshop.temp.bin.Sensor;
-import com.java.workshop.temp.bin.tempertureSensor;
+
 
 public class TempdataApplication {
     public static void main(String[] args) {
 
-    
+        Orgranaization vet =  new Orgranaization("Velalar Educational Trust", "Dindigul, Erode", 1234567890,
+                "vetias@dindulgmail.com", "www.vetias.com", 123456, LocalDate.of(2025, 07, 14));
+
+
+        System.out.println(vet.name());
+        System.out.println(vet.address());
+        System.out.println(vet.contactNumber());
+        System.out.println(vet.email());
+        System.out.println(vet.website());
+        System.out.println(vet.registrationNumber());
+        System.out.println(vet.registrationDate());
+;
+         
     }
 }
