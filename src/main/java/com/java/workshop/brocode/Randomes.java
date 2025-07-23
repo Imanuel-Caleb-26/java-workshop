052be138b8bd6d0d8ea6c1f7040b3c7b5c155f41
Imanuel-Caleb-26 <imanuelcaleb3@gmail.com>
2025-07-23 19:14:44 +0530

    Random

---
+++ src/main/java/com/java/workshop/brocode/Randomes.java
@@ -0,0 +1,24 @@
+package com.java.workshop.brocode;
+import java.util.Random;
+
+public class Randomes {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        
+        int num;
+        num = random.nextInt(1,6);
+
+        double num1;
+        num1 = random.nextDouble(1,7);
+
+        boolean num2;
+        num2 = random.nextBoolean();
+
+        System.out.println("The interger Random is: " + num );
+        System.out.println("The double Random is: " + num1 );
+        System.out.println("The Boolen Random is: " + num2 );
+
+    }
+
+}
