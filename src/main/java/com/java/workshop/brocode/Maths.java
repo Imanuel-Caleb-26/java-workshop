2016c69e7995eb89cc56ba48aeeb0329b2ca2c3f
Imanuel-Caleb-26 <imanuelcaleb3@gmail.com>
2025-07-24 21:24:59 +0530

    pi

---
+++ src/main/java/com/java/workshop/brocode/Maths.java
@@ -0,0 +1,21 @@
+package com.java.workshop.brocode;
+
+public class Maths {
+        public static void main(String[] args) {
+            Double result;
+
+            System.out.println(Math.PI);
+            System.out.println(Math.E);
+
+            result = Math.pow(2, 5); //power 
+            result = Math.abs(-5); //absolute value
+            result = Math.sqrt(9); //square root
+            result = Math.round(3.14); //round of 
+            result = Math.ceil(3.14); //celling
+            result = Math.floor(3.66); //floor
+            result = Math.max(10, 020); // max num
+            result = Math.min(10,20); //min num
+
+            System.out.println(result);
+        }
+}
