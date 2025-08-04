2e90f582cde3bccd27811b45edde7f9f64aa1087
Imanuel-Caleb-26 <imanuelcaleb3@gmail.com>
2025-08-04 20:46:20 +0530

    printf

--- src/main/java/com/java/workshop/basics/Printf.java
+++ src/main/java/com/java/workshop/basics/Printf.java
@@ -2,6 +2,7 @@ package com.java.workshop.basics;
 
 public class Printf {
         public static void main(String[] args) {
+            //formart specifier
             String name = "Imanuel";
             char fletter = 'I';
             int age = 19;
@@ -13,6 +14,16 @@ public class Printf {
             System.out.printf("Your age is %d \n", age);
             System.out.printf("Your Height is %f \n",height);
             System.out.printf("Is he Student %b \n",IsStudent);
+
+            //presition
+            double price1 = 90.1234;
+            double price2 = 199.8283;
+            double price3 = 23.3448;
+
+            System.out.printf("%f\n",price1);
+            System.out.printf("%.3f\n",price2);
+            System.out.printf("%.2f\n",price3);
+
 
         }
 }
