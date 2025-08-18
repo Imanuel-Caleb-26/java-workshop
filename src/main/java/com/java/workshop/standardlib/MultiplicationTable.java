31be5f7104e812237ea9d7eb76dc8914752a0d38
Imanuel-Caleb-26 <imanuelcaleb3@gmail.com>
2025-08-18 19:34:49 +0530

    multiple table

---
+++ src/main/java/com/java/workshop/standardlib/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package com.java.workshop.standardlib;
+
+public class MultiplicationTable {
+        public static void main(String[] args) {
+            int number = 2;
+    
+            System.out.println("Multiplication Table of " + number + ":");
+    
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(number + " x " + i + " = " + (number * i));
+            }
+        }
+    
+}
